bf8a1782ec7c9d8249efdbf29878763be397f16b
lonich <lanilor53@gmail.com>
2020-09-16 17:01:16 +0300

    Add /getJokes endpoint

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var createError = require('http-errors');
+var pgp = require('pg-promise')(/* options */)
+
+// Database connection
+const POSTGRES_USER = 'postgres'
+const POSTGRES_PASSWORD = 'root'
+const POSTGRES_HOST = 'localhost'
+const POSTGRES_PORT = '5432'
+var db = pgp('postgres://'+POSTGRES_USER+':'+POSTGRES_PASSWORD+'@'+POSTGRES_HOST+':'+POSTGRES_PORT+'/Filth') // TODO: change hardcoded constants to env variables
 
 
 router.get('/', function(req, res, next) {
     next(createError(403));
 });
 
+// Get all jokes from the database
 router.get('/getJokes', function(req, res, next) {
-    res.json({"jokes":[{"text":"123"},{"text":"432"}]});
+    db.any('SELECT * FROM public."Joke" AS jokes')
+        .then(function (data) {
+            if(data.length==0){
+                console.log('No jokes returned');
+                res.json({'success':true,'jokes':[]});
+            }else{
+                console.log('Successfully returned some jokes, count: '+data.length);
+                res.json({'success':true,'jokes':data});
+            }
+        })
+        .catch(function (error) {
+            console.log('ERROR: ', error);
+            res.json({'success':false,'error':error});
+        })
 });
 
 
