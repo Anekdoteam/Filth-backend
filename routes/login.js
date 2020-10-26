939d269fa8ddbd2563f2de648abfbeed30ead506
lonich <lanilor53@gmail.com>
2020-10-26 11:24:24 +0300

    Fixed issues found by linter, mostly refactoring.

--- routes/login.js
+++ routes/login.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var pgp = require('pg-promise')(/* options */);
 
 router.use(bodyParser.urlencoded({extended: true}));
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
 const POSTGRES_USER = process.env.PGUSER.replace('/["]+/', '');
 const POSTGRES_PASSWORD = process.env.PGPW.replace('/["]+/', '');
@@ -17,56 +17,56 @@ const POSTGRES_PORT = process.env.PGPORT.replace('/["]+/', '');
 var db = pgp('postgres://'+POSTGRES_USER+':'+POSTGRES_PASSWORD+'@'+POSTGRES_HOST+':'+POSTGRES_PORT+'/Filth');
 
 passport.use(new LocalStrategy((username, password, done) => {
-	console.log("Data for strategy: " + username + ", " + password);
-	db.oneOrNone('SELECT "password" FROM "public"."User" WHERE "username" = $1;', username).then(function (data, credentials) {
-		console.log("username: " + username);
-		if (!data) {
-			console.log("works");
-			return done(null, false, {message: "User not found"});
-		} else {
-			console.log("Type of cred: " + typeof(credentials.password) + " and type of dbpw: " + typeof(data));
-			
-			console.log("Passwords: " + data.valueOf() + " and " + credentials.password.valueOf());
-			
-			if (data.trim() == credentials.password.trim()) {
-				console.log("Password correct");
-				return done(null, {un: username, pw: data.trim()});
-			} else {
-				console.log("Password incorrect.");
-				data = null;
-				return done(null, false, {message: "Password incorrect"});
-			}
-			
-			//if (data)
-		}
-	}.bind(null, password)).catch((error) => {
-		console.log('ERROR: ', error);
-		res.json({'success': false, 'error': error});
-	})
+  console.log("Data for strategy: " + username + ", " + password);
+  db.oneOrNone('SELECT "password" FROM "public"."User" WHERE "username" = $1;', username).then(function (data, credentials) {
+    console.log("username: " + username);
+    if (!data) {
+      console.log("works");
+      return done(null, false, {message: "User not found"});
+    } else {
+      console.log("Type of cred: " + typeof(credentials.password) + " and type of dbpw: " + typeof(data));
+
+      console.log("Passwords: " + data.valueOf() + " and " + credentials.password.valueOf());
+
+      if (data.trim() == credentials.password.trim()) {
+        console.log("Password correct");
+        return done(null, {un: username, pw: data.trim()});
+      } else {
+        console.log("Password incorrect.");
+        data = null;
+        return done(null, false, {message: "Password incorrect"});
+      }
+
+      //if (data)
+    }
+  }.bind(null, password)).catch((error) => {
+    console.log('ERROR: ', error);
+    return done(null, false, {message: "An exception has occured."});
+  });
 }));
 
 passport.serializeUser((user, done) => {
-	done(null, user);
+  done(null, user);
 });
 
 passport.deserializeUser((user, done) => {
-	done(null, user);
+  done(null, user);
 });
 
 router.post('/', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true}), (req, res, next) => {
-	console.log("Request body: " + JSON.stringify(req.body));
-	
-	//res.redirect('/');
-	res.json({"ok": true});
+  console.log("Request body: " + JSON.stringify(req.body));
+
+  //res.redirect('/');
+  res.json({"ok": true});
 });
 
 router.get('/', passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true}), (req, res, next) => {
-	if (req.isAuthenticated()) {
-		console.log("You are authenticated"); 
-	}
-	console.log("Request body: " + JSON.stringify(req.body));
+  if (req.isAuthenticated()) {
+    console.log("You are authenticated"); 
+  }
+  console.log("Request body: " + JSON.stringify(req.body));
 
-	res.json({"ok": true});
+  res.json({"ok": true});
 });
 
 
